d8b055d24eb9d76d69ed03c772f5eeb72496d921
mj10021 <jamesthespeedy@gmail.com>
2022-08-03 14:04:17 -0400

    improved wipe and retract

--- vertex-delete.py
+++ vertex-delete.py
@@ -1,17 +1,18 @@
 import re
 
-with open(r'file location', 'r') as f:
+with open(file_location, 'r') as f:
     data = f.read()
 
 target_x = 88.305
 target_y = 58.12
 max_dist = 3.5
-retraction = 1
-z_lift = .2
+retraction = 0.4
+de_retraction_ratio = 1.25
+z_lift = .4
 
 layer_list = [x.split("\n") for x in data.split('AFTER_LAYER_CHANGE')]
 for layer_num, layer in enumerate(layer_list):
-    if layer_num < 4 or layer_num >= 18:
+    if layer_num < 4 or layer_num >= 14:
         continue
     for i, cmd in enumerate(layer):
         if type(cmd) is not str:
@@ -25,8 +26,21 @@ for layer_num, layer in enumerate(layer_list):
             if backstep is None:
                 continue
             back_x, back_y = [float(x__) for x__ in backstep.groups()]
-            layer_list[layer_num][i] = 'G1 X%f Y%f E%f' % (back_x, back_y, retraction * -1)
-            insertion = ['G91','G1 Z%f' (z_lift),'G90','G1 X%f Y%f' % (x, y),'G91','G1 Z-%f E%f' % (z_lift, retraction),'G90']
+            layer_list[layer_num][i] = ';MOD_START'
+            
+            insertion = [
+                'G1 X%f Y%f E-%f;reverse move and retract' % (back_x, back_y, retraction),
+                'G91;relative positioning',
+                'G1 Z%f;lift z' % (z_lift),
+                'G90;absolute positioning',
+                'G1 X%f Y%f;next move' % (x, y),
+                'G91;relative positioning',
+                'G1 Z-%f E%f;resume z and de-retract' % (z_lift, retraction * de_retraction_ratio),
+                'G90;absolute positioning',
+                'G4 P50;dwell 50ms',
+                ';MOD_END',
+                ]
+
             for item in insertion[::-1]:
                 layer_list[layer_num].insert(i + 1 ,item)
             
